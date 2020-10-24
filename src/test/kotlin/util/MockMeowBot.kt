e5093f53ab2296f362f322233972873ce34cd93d
Lauren Wise <lvvise@icloud.com>
2020-10-23 23:41:45 -0400

    Attempt to test the alignment listener

--- src/test/kotlin/util/MockMeowBot.kt
+++ src/test/kotlin/util/MockMeowBot.kt
@@ -2,6 +2,7 @@ package util
 
 import com.lwise.listeners.Listener
 import com.lwise.listeners.messages.MessageListener
+import com.lwise.listeners.reactions.ReactionListener
 
 @Suppress("UNCHECKED_CAST")
 class MockMeowBot(val listeners: List<Listener<*>>) {
@@ -14,5 +15,11 @@ class MockMeowBot(val listeners: List<Listener<*>>) {
             return it.getResponseMessage()
         }
         return null
+    }
+
+    fun firesOnReaction(emojiName: String): Boolean {
+        val listener = (listeners as List<ReactionListener>)
+            .firstOrNull { it.isTriggered(emojiName) }
+        return listener != null
     }
 }
