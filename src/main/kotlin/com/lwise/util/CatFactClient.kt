4b4ac4d51f7fc3e69204f403cc4bc844a61930a1
Lauren Wise <lvvise@icloud.com>
2020-11-20 23:34:57 -0500

    Meowbot retrieves a RANDOM cat fact

--- src/main/kotlin/com/lwise/util/CatFactClient.kt
+++ src/main/kotlin/com/lwise/util/CatFactClient.kt
@@ -12,6 +12,7 @@ object CatFactClient {
         .scheme("https")
         .host("cat-fact.herokuapp.com")
         .addPathSegment("facts")
+        .addPathSegment("random")
         .addQueryParameter("amount", "1")
         .build()
 
@@ -24,7 +25,7 @@ object CatFactClient {
             val response = client.newCall(request).execute()
             val result = JSONObject(response.body!!.string())
             log(this::class.java.name, result.toString())
-            return result.getJSONArray("all").getJSONObject(0).get("text").toString()
+            return result.get("text").toString()
         } catch (exception: IOException) {
             logException(this::class.java.name, "Cannot retrieve fact from $queryFactUrl", exception)
             return null
